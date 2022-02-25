e8c26b52634313ab8ecceac33da10c5a15d82bfb
LouetR <romainlouet@romsworld.net>
2022-02-25 07:37:44 +0100

    migrate task_edit view, install framework-extra-bundle

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -44,4 +44,25 @@ class TaskController extends AbstractController
             ['form' => $form->createView()],
         );
     }
+
+    #[Route('/tasks/{id}/edit', name: 'task_edit')]
+    public function editAction(Task $task, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'task' => $task,
+        ]);
+    }
 }
