5be9494922a41a79535ba4a5d27778c9ee476dff
LouetR <romainlouet@romsworld.net>
2022-02-27 07:25:35 +0100

    migrate users_list view and route, add logout route and improve layout

---
+++ src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/users', name: 'user_list')]
+    public function listAction(UserRepository $userRepository): Response
+    {
+        return $this->render(
+            'user/list.html.twig',
+            ['users' => $userRepository->findAll()],
+        );
+    }
+
+}
